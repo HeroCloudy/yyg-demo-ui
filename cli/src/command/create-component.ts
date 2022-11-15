64df3b0492c1038bb01e11c0033be547fd015105
liuyun <yunliuly@cn.ibm.com>
2022-11-15 14:26:38 +0800

    08搭建组件库cli基础框架-交互提示

--- cli/src/command/create-component.ts
+++ cli/src/command/create-component.ts
@@ -1,3 +1,33 @@
+import inquirer, { QuestionCollection } from 'inquirer'
+
+// 交互提示
+const questions: QuestionCollection = [
+  {
+    name: 'componentName',
+    message: 'Input the component name: ',
+    default: ''
+  },
+  {
+    name: 'description',
+    message: 'Input the component description: ',
+    default: ''
+  },
+  {
+    type: 'list',
+    name: 'componentType',
+    message: 'Choose the component type: ',
+    choices: [
+      'tsx', 'vue'
+    ]
+  }
+]
+
+const createNewComponent = (componentName: string, description: string, componentType: string) => {
+  console.log(componentName, description, componentType)
+}
+
 export const createComponent = () => {
-  console.log('创建新组建')
+  inquirer.prompt(questions).then(({ componentName, description, componentType }) => {
+    createNewComponent(componentName, description, componentType)
+  })
 }
