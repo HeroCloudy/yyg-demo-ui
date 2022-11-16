a560c4f54309c38fd4273d4a1cd97e678210892e
liuyun <yunliuly@cn.ibm.com>
2022-11-16 10:40:16 +0800

    11组件库发布

---
+++ packages/yyg-demo-ui/vite.config.ts
@@ -0,0 +1,33 @@
+import { defineConfig } from 'vite'
+import vue from '@vitejs/plugin-vue'
+import * as path from 'path'
+import VueJsx from '@vitejs/plugin-vue-jsx'
+import viteDts from 'vite-plugin-dts'
+
+export default defineConfig({
+  plugins: [
+    vue(),
+    VueJsx(),
+    viteDts({
+      insertTypesEntry: true,
+      staticImport: true,
+      skipDiagnostics: true
+    })
+  ],
+  build: {
+    lib: {
+      entry: path.resolve(__dirname, './index.ts'),
+      name: 'yyg-demo-ui',
+      fileName: format => `yyg-demo-ui.${format}.js`
+    },
+    outDir: path.resolve(__dirname, '../../lib'),
+    rollupOptions: {
+      external: ['vue'],
+      output: {
+        globals: {
+          vue: 'Vue'
+        }
+      }
+    }
+  }
+})
