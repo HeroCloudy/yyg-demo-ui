cc5e2cfc6f49c1100214c4ff195b0bf2e862dacb
liuyun <yunliuly@cn.ibm.com>
2022-12-03 23:44:50 +0800

    feat: 组件库文档上下文路径

--- docs/.vitepress/config.ts
+++ docs/.vitepress/config.ts
@@ -1,6 +1,7 @@
 import { DefaultTheme, defineConfig } from 'vitepress'
 import { componentPreview, containerPreview } from "@vitepress-demo-preview/plugin"
 import { components } from '../components'
+import minimist from 'minimist'
 
 const nav: DefaultTheme.NavItem[] = [
   { text: '指南', link: '/guide/' },
@@ -24,11 +25,14 @@ const sidebar: DefaultTheme.Sidebar = {
   }]
 }
 
+const argv = minimist(process.argv.slice(2))
+const build = argv.build || false
+
 export default defineConfig({
-  title: 'yyg-admin-ui',
+  title: 'yyg-demo-ui',
   description: 'YYG Vue3企业级中后台组件库',
   lang: 'cn-ZH',
-  base: '/',
+  base: build ? '/yyg-demo-ui/' : '/',
   lastUpdated: true,
   themeConfig: {
     logo: '/logo.png',
